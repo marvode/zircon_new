ebcd13e69dcb255d335a78b3c6c49f531ea8214e
Marvellous Odemwingie <odemwingiemarv@gmail.com>
2021-11-05 21:58:01 +0100

    modified the orders table to accept guests

--- database/migrations/2021_10_29_121200_create_orders_table.php
+++ database/migrations/2021_10_29_121200_create_orders_table.php
@@ -16,7 +16,7 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('user_id')->nullable();
             $table->string('name');
             $table->enum('status', Order::getOrderStatusList())->default(Order::ORDER_PENDING);
             $table->float('price');
